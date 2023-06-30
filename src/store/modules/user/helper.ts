a7fe6604a47d14e2af827af2d7430a48b3a344c7
Liskie <wyj125869@126.com>
2023-06-30 11:57:37 +0800

    Modified username.

--- src/store/modules/user/helper.ts
+++ src/store/modules/user/helper.ts
@@ -17,7 +17,7 @@ export function defaultSetting(): UserState {
     userInfo: {
       avatar: 'https://raw.githubusercontent.com/Chanzhaoyu/chatgpt-web/main/src/assets/avatar.jpg',
       name: 'LLMs United',
-      description: 'BLCU-iCALL" >GitHub</a>',
+      description: 'BLCU-iCALL',
     },
   }
 }
